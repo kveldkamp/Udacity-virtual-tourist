57c6312b13b9fd88d1dbe033eb311cf3ef36ccab
Kevin Veldkamp <kveldkamp@veyo.com>
2019-07-03 18:35:05 -0700

    Initial Commit

---
+++ virtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by Kevin Veldkamp on 7/3/19.
+//  Copyright © 2019 kevin veldkamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
